2006879dfd9cece76a4171a23e8987a4ec6f2064
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-11-15 02:48:51 +0100

    add routes to edit and delete portfolio, add scraping for brazilian stocks (npm package)

--- routes/watchList.routes.js
+++ routes/watchList.routes.js
@@ -3,12 +3,11 @@ const getFiiData = require('../utils/getFiiData');
 const asyncFor = require('../utils/asyncForEach');
 const WatchList = require('../models/WatchList.model');
 const throwError = require('../utils/throwError');
+const statusInvest = require('statusinvest');
 
 //POST: Create a watchlist for fiis and stocks of the brazilian market
 router.post('/', async (req, res, next) => {
-	const { fiis } = req.body;
-
-	const { stocks } = req.body;
+	const { fiis, stocks } = req.body;
 
 	const owner = req.payload._id;
 
@@ -28,24 +27,65 @@ router.post('/', async (req, res, next) => {
 //GET: Scrap statusinvest.com for the information about FIIs
 //     Will scrap stocks too in the future or use a package that exists for stocks
 router.get('/', async (req, res, next) => {
-	const { _id: id } = req.payload;
+	const owner = req.payload._id;
 
 	try {
-		const watchList = await WatchList.findOne({ owner: id });
+		const watchList = await WatchList.findOne({ owner });
 
-		throwError(!watchList, 400, "You still don't have a watch list", )
-	
-		const portfolio = [];
-	
-		await asyncFor(watchList.fiis, getFiiData, portfolio);
-	
+		throwError(!watchList, 400, "You still don't have a watch list");
+
+		const portfolio = { fiis: [], stocks: [] };
+
+		await asyncFor(watchList.fiis, getFiiData, portfolio.fiis);
+
+		if (watchList.stocks.length !== 0) {
+			const allStocks = await statusInvest.getStocksInfo();
+
+			portfolio.stocks = allStocks.filter(stock => watchList.stocks.includes(stock.Ativo));
+		}
+
 		res.status(200).json(portfolio);
 	} catch (err) {
-		next(err)
+		next(err);
 	}
 });
 
-// PUT: Update the existing watchList 
-router.put('/')
+// PUT: Update the existing watchList
+router.put('/', async (req, res, next) => {
+	const owner = req.payload._id;
+
+	const { fiis, stocks } = req.body;
+
+	try {
+		if (fiis.length === 0 && stocks.length === 0)
+			throwError(true, 400, 'You need at least one fiis and/or stock', 'Updating watch list');
+
+		const watchList = await WatchList.findOneAndUpdate(
+			{ owner },
+			{ fiis, stocks },
+			{ new: true, runValidators: true }
+		);
+
+		throwError(!watchList, 400, 'You need a WatchList to update', 'Updating watch list');
+
+		res.status(200).json(watchList);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.delete('/', async (req, res, next) => {
+	const owner = req.payload._id;
+
+	try {
+		const { deletedCount } = await WatchList.deleteOne({ owner });
+
+		throwError(!deletedCount, 400, "You don't have a WatchList", 'Deleting a watchlist');
+
+		res.status(204).json();
+	} catch (err) {
+		next(err);
+	}
+});
 
 module.exports = router;
--- utils/scrapeTesting.js
+++ utils/scrapeTesting.js
@@ -1,15 +1,25 @@
 const asyncForEach = require('./asyncForEach')
 const getFiiData = require('./getFiiData')
 const axios = require('axios')
+const statusInvest = require('statusinvest')
 
 const portfolio = []
 
-const test = ["HGLG11"]
+// const test = ["HGLG11"]
 
-asyncForEach(test, getFiiData, portfolio)
-  .then(() => {
-    console.log(portfolio)
-  })
-  .catch((err) => {
-    console.log('Ih deu merda', err)
-  })
+// asyncForEach(test, getFiiData, portfolio)
+//   .then(() => {
+//     console.log(portfolio)
+//   })
+//   .catch((err) => {
+//     console.log('Ih deu merda', err)
+//   })
+
+const stockScrapping = async () => {
+  const testStocks = ["TRPL4", "TAEE11"]
+
+  const stocks = await statusInvest.getStocksInfo()
+  console.log(stocks.filter(stock => testStocks.includes(stock.Ativo)))
+}
+
+stockScrapping()
