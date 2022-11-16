f3b6d0c04276fb411b63979272fa6d51fd309ae0
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-11-16 01:34:38 +0100

    removed unecessary imports

--- routes/watchList.routes.js
+++ routes/watchList.routes.js
@@ -1,21 +1,16 @@
 const router = require('express').Router();
-const getFiiData = require('../utils/getFiiData');
-const asyncFor = require('../utils/asyncForEach');
-const WatchList = require('../models/WatchList.model');
-const throwError = require('../utils/throwError');
-const statusInvest = require('statusinvest');
 
 const WatchControllers = require('../controllers/watch.controllers')
 
 //POST: Create a watchlist for fiis and stocks of the brazilian market
 router.post('/', WatchControllers.create);
 
-//GET: Scrap statusinvest.com for the information about FIIs
-//     Will scrap stocks too in the future or use a package that exists for stocks
+//GET: Scrap statusinvest.com for the information about FIIs and Stocks (using npm package)
 router.get('/', WatchControllers.get);
 
 // PUT: Update the existing watchList
 router.put('/', WatchControllers.update);
+
 
 router.delete('/', WatchControllers.delete);
 
